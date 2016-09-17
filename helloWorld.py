17653e560146ce234de9ab30a3d85dc931516316
Will Sergeant <crgk71@gmail.com>
2016-09-17 18:29:22 -0400

    Really just a bunch of junk python code.

---
+++ helloWorld.py
@@ -0,0 +1,7 @@
+print("This math", 5+2)
+age = 30
+for x in range(0,10):
+    print (x)
+    if (x < 9):
+        print('y')
+        
---
+++ pygame1.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+
+import pygame
+from pygame.locals import *
+
+def main():
+    #initialize screen
+    pygame.init()
+    screen = pygame.display.set_mode((150,50))
